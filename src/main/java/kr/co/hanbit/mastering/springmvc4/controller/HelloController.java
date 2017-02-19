49badf1f3f0d441aa1746d1c19cbe171de088d2b
Sangjinny <sangjinny28@gmail.com>
2017-02-19 16:43:33 +0900

    2017-02-19

--- src/main/java/kr/co/hanbit/mastering/springmvc4/controller/HelloController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/controller/HelloController.java
@@ -1,8 +1,8 @@
 package kr.co.hanbit.mastering.springmvc4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Jeon on 2017-02-05.
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
     @RequestMapping("/")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from the controller");
         return "resultPage";
     }
 
