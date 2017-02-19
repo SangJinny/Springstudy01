eaf008a47df2275d0218a539cb90a009dd458518
Sangjinny <sangjinny28@gmail.com>
2017-02-19 18:04:09 +0900

    twitterstart1

--- src/main/java/kr/co/hanbit/mastering/springmvc4/controller/TweetController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/controller/TweetController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,23 +12,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Created by Jeon on 2017-02-05.
+ * Created by Jeon on 2017-02-19.
  */
-@Controller
-public class HelloController {
+public class TweetController {
     @Autowired
     private Twitter twitter;
 
-    @RequestMapping("/")
+    @RequestMapping("/aa")
     public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
-        List<Tweet> tweets = searchResults.getTweets();
-        model.addAttribute("search", search);
+        List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
         model.addAttribute("tweets",tweets);
-
+        //String text = searchResults.getTweets().get(0).getText();
+       // model.addAttribute("message",text);
         return "resultPage";
-
+        //model.addAttribute("message", "Hello from the controller");
+        //return "resultPage";
 
     }
-
 }
