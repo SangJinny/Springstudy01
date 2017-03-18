4c44f42762675652b5d429d442afad47737f4ed9
Sangjinny <sangjinny28@gmail.com>
2017-03-18 19:32:06 +0900

    add validation message and i18n

--- src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebConfiguration.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebConfiguration.java
@@ -5,13 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.nio.charset.Charset;
 import java.time.LocalDate;
+import java.util.logging.Filter;
 
 /**
  * Created by Jeon on 2017-03-01.
@@ -37,6 +42,18 @@ public class WebConfiguration extends WebMvcConfigurerAdapter{
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         return localeChangeInterceptor;
+    }
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return (Filter) characterEncodingFilter;
     }
 
     @Override
--- src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -19,13 +20,19 @@ import java.util.Locale;
 @Controller
 public class ProfileController {
 
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+
+        return KRLocalDateFormatter.getPattern(locale);
+    }
+
     @RequestMapping("/profile")
     public String displayProfile(ProfileForm profileForm) {
 
         return "profile/profilePage";
     }
 
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    @RequestMapping(value = "/profile", params={"save"}, method = RequestMethod.POST)
     public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()) {
@@ -36,9 +43,17 @@ public class ProfileController {
         return "redirect:/profile";
     }
 
-    @ModelAttribute("dateFormat")
-    public String localeFormat(Locale locale) {
+    @RequestMapping(value = "/profile", params={"addTaste"})
+    public String addRow(ProfileForm profileForm) {
+        profileForm.getTastes().add(null);
+        return "profile/profilePage";
+    }
 
-        return KRLocalDateFormatter.getPattern(locale);
+    @RequestMapping(value = "/profile", params={"removeTaste"})
+    public String removeRow(ProfileForm profileForm, HttpServletRequest req){
+
+        Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return "profile/profilePage";
     }
 }
