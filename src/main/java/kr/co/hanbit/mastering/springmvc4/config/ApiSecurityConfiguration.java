403a8a3bcb1eaaa6663a42990a736cd7974f976d
Sangjinny <sangjinny28@gmail.com>
2017-04-05 21:11:05 +0900

    spring security applied

--- src/main/java/kr/co/hanbit/mastering/springmvc4/config/ApiSecurityConfiguration.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/config/ApiSecurityConfiguration.java
@@ -2,6 +2,7 @@ package kr.co.hanbit.mastering.springmvc4.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,9 +13,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * Created by Jeon on 2017-04-02.
  */
 @Configuration
+@Order(1)
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter{
     /* 더 세밀한 보안을 위해 만든 coufiguration */
+
+    /*
+     * APISecurityConfiguration: 첫 번째 구성이며, 기본 인증으로 RESTful 엔드포인트를 보호한다.
+     * WebSecurityConfiguration: 애플리케이션의 나머지 부분에 대한 로그인 폼을 구성한다.
+     * */
+    
     @Autowired
     public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -26,6 +34,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .antMatcher("/api/**")
+                .httpBasic().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET).hasRole("USER")
+                .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                .anyRequest().authenticated();
+        /*
         http.httpBasic()
                 .and().csrf().disable()
                 .authorizeRequests()
@@ -35,5 +54,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
+                */
     }
 }
---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package kr.co.hanbit.mastering.springmvc4.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Jeon on 2017-04-05.
+ */
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /*
+     * APISecurityConfiguration: 첫 번째 구성이며, 기본 인증으로 RESTful 엔드포인트를 보호한다.
+     * WebSecurityConfiguration: 애플리케이션의 나머지 부분에 대한 로그인 폼을 구성한다.
+     * */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .formLogin()
+                .defaultSuccessUrl("/profile")
+                .and()
+                .logout().logoutSuccessUrl("/login")
+                .and()
+                .authorizeRequests().antMatchers("/webjars/**", "/login").permitAll()
+                .anyRequest().authenticated();
+    }
+}
